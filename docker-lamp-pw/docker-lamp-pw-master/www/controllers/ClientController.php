279d2cc823599186b4b901fa876906014f375d52
barcelos1803 <vinibarcelos18@gmail.com>
2022-09-04 18:06:37 -0300

    apenas alguns alinhamentos

--- docker-lamp-pw/docker-lamp-pw-master/www/controllers/ClientController.php
+++ docker-lamp-pw/docker-lamp-pw-master/www/controllers/ClientController.php
@@ -1,40 +1,40 @@
 <?php 
 
-class ClientController{
+    class ClientController{
 
-    public function index(){
+        public function index(){
 
-    }
-    public function register(){
-        require_once('views/templates/header.php');
-        require_once('views/client/register.php');
-        require_once('views/templates/footer.php');
-    }
-
-    public function registerView(){
-        if(isset($_POST['accept'])){
-            $accept = true;
-            $acceptView = "Termo aceito";
-        }else{
-            $accept = false;
-            $acceptView = "Termo não aceito";
+        }
+        public function register(){
+            require_once('views/templates/header.php');
+            require_once('views/client/register.php');
+            require_once('views/templates/footer.php');
         }
 
-        $arrayClient = array(
-        'name' => $_POST['name'],
-        'email' => $_POST['email'],
-        'phone' => $_POST['phone'],
-        'gender' => $_POST['gender'],
-        'accept' => $accept,
-        'acceptView' => $acceptView
-        );
+        public function registerView(){
+            if(isset($_POST['accept'])){
+                $accept = true;
+                $acceptView = "Termo aceito";
+            }else{
+                $accept = false;
+                $acceptView = "Termo não aceito";
+            }
 
-        require_once('views/templates/header.php');
-        require_once('views/client/registerView.php');
-        require_once('views/templates/footer.php');
-        
+            $arrayClient = array(
+                'name' => $_POST['name'],
+                'email' => $_POST['email'],
+                'phone' => $_POST['phone'],
+                'gender' => $_POST['gender'],
+                'accept' => $accept,
+                'acceptView' => $acceptView
+            );
+
+            require_once('views/templates/header.php');
+            require_once('views/client/registerView.php');
+            require_once('views/templates/footer.php');
+            
+        }
     }
-}
 
 
 ?>
--- docker-lamp-pw/docker-lamp-pw-master/www/controllers/SiteController.php
+++ docker-lamp-pw/docker-lamp-pw-master/www/controllers/SiteController.php
@@ -1,31 +1,32 @@
 <?php
 
 
-class SiteController{
+    class SiteController{
 
-    public function home(){
-        require_once('views/templates/header.php');
-        require_once('views/site/home.html');
-        require_once('views/templates/footer.php');
+        public function home(){
+            require_once('views/templates/header.php');
+            require_once('views/site/home.html');
+            require_once('views/templates/footer.php');
+        }
+        public function sobreNos(){
+            require_once('views/templates/header.php');
+            require_once('views/site/sobreNos.html');
+            require_once('views/templates/footer.php');
+        }
+        public function feminino(){
+            require_once('views/templates/header.php');
+            require_once('views/site/feminino.html');
+            require_once('views/templates/footer.php');
+        }
+        public function infantil(){
+            require_once('views/templates/header.php');
+            require_once('views/site/infantil.html');
+            require_once('views/templates/footer.php');
+        }
+        public function masculino(){
+            require_once('views/templates/header.php');
+            require_once('views/site/masculino.html');
+            require_once('views/templates/footer.php');
+        }
     }
-    public function sobreNos(){
-        require_once('views/templates/header.php');
-        require_once('views/site/sobreNos.html');
-        require_once('views/templates/footer.php');
-    }
-    public function feminino(){
-        require_once('views/templates/header.php');
-        require_once('views/site/feminino.html');
-        require_once('views/templates/footer.php');
-    }
-    public function infantil(){
-        require_once('views/templates/header.php');
-        require_once('views/site/infantil.html');
-        require_once('views/templates/footer.php');
-    }
-    public function masculino(){
-        require_once('views/templates/header.php');
-        require_once('views/site/masculino.html');
-        require_once('views/templates/footer.php');
-    }
-}
+?>
